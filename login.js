77043e52e5318a10ba325361de0f0fb355ad0013
liuliu250 <10755059@qq.com>
2025-09-06 19:31:59 +0800

    1

--- login.js
+++ login.js
@@ -1,28 +1,41 @@
-const loginBtn = document.getElementById("login_btn");
-const registerBtn = document.getElementById("register_btn");
-function getBody(){
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+
+const loginBtn = document.getElementById('login_btn')
+const registryBtn = document.getElementById('registry_btn')
+
+function getBody() {
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
     return { username, password }
 }
-function fetchData(url, body){
+
+function fetchData(url, body) {
     return fetch(url, {
         method: 'POST',
         headers: {
             "Content-Type": 'application/json;charset=UTF-8'
         },
         body: JSON.stringify(body)
-    });
+    })
 }
-loginBtn.addEventListener("click",  async () => {
-    const body = getBody();
-    console.log(body);
-    const response = await fetchData("http://localhost:3000/login", body);
-    const data = await response.json();
-})
-registerBtn.addEventListener("click",  async () => {
-    const body = getBody();
-    const response = await fetchData("http://localhost:3000/registry", body);
-    const data = await response.json();
 
+async function fetchUser() {
+    const response = await fetch('http://localhost:3000/users')
+    const data = await response.json()
+    const userList = document.getElementById('userList')
+    userList.innerHTML = data.users.map(item => `<div>${item}</div>`).join('')
+}
+fetchUser()
+
+loginBtn.addEventListener('click', async () => {
+    const body = getBody()
+    const response = await fetchData('http://localhost:3000/login', body)
+    const data = await response.json()
+    const loginUsernName = document.getElementById('loginUsernName')
+    loginUsernName.innerText = data.username
+})
+
+registryBtn.addEventListener('click', async () => {
+    const body = getBody()
+    const response = await fetchData('http://localhost:3000/registry', body)
+    const data = await response.json()
 })
